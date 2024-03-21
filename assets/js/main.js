f0d60c1871b23b59dcf175d8fd4953785a01283f
unknown <gouravbishnoi429@gmail.com>
2024-03-21 10:08:54 +0530

    add ellipse

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,31 @@
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
-    document.querySelector(".menu").classList.toggle("cross")
-  }
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
+}
 
-//   document.addEventListener("DOMContentLoaded", function () {
-//     window.addEventListener("load", function () {
-//       setTimeout(function () {
-//         let loadingOverlay = document.getElementById("loadingOverlay");
-//         loadingOverlay.style.display = "none";
-//         document.querySelector("body").classList.remove("overflow_hidden");
-//       }, 3000);
-//     });
-//   });
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      let loadingOverlay = document.getElementById("loadingOverlay");
+      loadingOverlay.style.display = "none";
+      document.querySelector("body").classList.remove("overflow_hidden");
+    }, 3000);
+  });
+});
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
